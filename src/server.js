1221cbf896763980b5d50601fcbff2451ff9cb7b
ruong32 <trongruong99@gmail.com>
2020-04-21 23:59:47 +0700

    deploy on heroku

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import http from 'http';
+import path from 'path';
+import bodyParser from 'body-parser';
+import initRoutes from '../src/routes/routes';
+import connectDB from '../src/config/connectDB';
+const PORT = process.env.PORT || 3299
+
+connectDB();
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+initRoutes(app);
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
